6746f9f2fd7a123b17304228531acdbfb07462db
ashfaq_ummer <ashfaqummerin@gmail.com>
2023-05-29 20:46:08 +0530

    changes in global css

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -23,13 +23,13 @@ const Nav = () => {
     <nav className="flex-between w-full mb-16 pt-3">
       <Link href="/" className="flex gap-2 flex-center">
         <Image
-          src="/assets/images/logo.svg"
-          alt="Promtopia logo"
+          src="/assets/icons/terminal.svg"
+          alt="Promtify logo"
           width={30}
           height={30}
           className="object-contain"
         />
-        <p className="logo_text">Promptopia</p>
+        <p className="logo_text">Promptify</p>
       </Link>
 
       {/*Desktop navigation*/}
